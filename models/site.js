a76d6a6ccfbb7a387ce21f5563fa4300bcc3143f
endiawilliams <endiawilliams1@gmail.com>
2020-10-24 17:21:27 -0500

    Initial commit - db setup with Sequelize

---
+++ models/site.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class site extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.site.hasMany(models.post)
+    }
+  };
+  site.init({
+    locationId: DataTypes.INTEGER,
+    type: DataTypes.STRING,
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'site',
+  });
+  return site;
+};
