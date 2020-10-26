01865c227b5354697723131f2933d0a4c0550f01
endiawilliams <endiawilliams1@gmail.com>
2020-10-26 16:38:57 -0500

    Added forms to index page and made other minor changes to EJS pages.

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.get('/profile', isLoggedIn, (req, res) => {
 });
 
 app.use('/auth', require('./routes/auth'));
+app.use('/', require('./routes/index'));
 
 var server = app.listen(process.env.PORT || 3000, ()=> console.log(`🎧You're listening to the smooth sounds of port ${process.env.PORT || 3000}🎧`));
 
